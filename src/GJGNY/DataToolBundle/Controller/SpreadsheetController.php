40e44b63a7f4b2a1c6613f363cfe21f6d6e92345
Pat Haggerty <haggertypat@gmail.com>
2012-09-28 15:38:43 -0400

    county removal utility

--- src/GJGNY/DataToolBundle/Controller/SpreadsheetController.php
+++ src/GJGNY/DataToolBundle/Controller/SpreadsheetController.php
@@ -71,7 +71,10 @@ class SpreadsheetController extends Controller
         {
             if(count($county->getLeads()) == 0) {
                 $response .= $county->__toString()." has 0 leads<br/>";
+                $em->remove($county);
             }
+            $em->flush();
+            $em->clear();            
         }
         
         return $response;
